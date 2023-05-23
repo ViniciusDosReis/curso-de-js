19b2e49e0ed5b970af97a112d67f289f1c4fea23
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-05-23 17:01:38 -0300

    Initial commit

---
+++ js/aula01.js
@@ -0,0 +1,14 @@
+/* 
+arquivo js externo
+*/
+
+document.write("Olá mundo!")
+console.log("Outro olá mundo!") // comentario
+// alert("Olá mundo com Alerta")
+
+let mensagem = "Minha mensagem"
+let mensagem2 = 'Outra mensagem'
+let meuPeso = 52
+let minhaAltura = 1.65
+let minhaIdade = 16
+let ehDoador = true
