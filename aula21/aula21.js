1710057ca43739b238abf658ecbab1442bd3eabe
Vinicius de Andrade <107960098+ViniciusDosReis@users.noreply.github.com>
2023-07-05 17:05:15 -0300

    função com parâmetros rest

---
+++ aula21/aula21.js
@@ -0,0 +1,11 @@
+function canal() {
+    let n1 = 10
+    let n2 = 2
+    let res = n1 * n2
+    return "Impar"
+    return "Par"
+    console.log("Fim da funcaos")
+}
+
+let res = canal()
+console.log(res)
